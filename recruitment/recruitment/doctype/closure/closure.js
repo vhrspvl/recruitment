6e771ea587262076d9a8ee4da7fdca48252d460c
Abdulla <hereabdulla@gmail.com>
2017-05-24 20:08:39 +0530

    changes in closure to align with territory

--- recruitment/recruitment/doctype/closure/closure.js
+++ recruitment/recruitment/doctype/closure/closure.js
@@ -2,82 +2,83 @@ cur_frm.add_fetch("project", "cpc", "cpc");
 cur_frm.add_fetch("customer", "customer_owner", "bde");
 
 frappe.ui.form.on('Closure', {
-    'onload_post_render': function (frm) {
-      //me = $(cur_frm.fields_dict.payment_terms);
-      $(frm.fields_dict.payment_terms).addClass('test');
-    },
+  'onload_post_render': function(frm) {
+    //me = $(cur_frm.fields_dict.payment_terms);
+    //  frm.get_field("offer_letter").$input
+    //  .addClass('btn-primary')
+    //$(cur_frm.fields_dict.offer_letter).addClass('test');
+  },
 
-    refresh: function (frm) {
-      if (frm.perm[0].write) {
-        if (frm.doc.status == "Pending for CSL") {
-          frm.add_custom_button(__("Confirm CSL"), function () {
-            frm.set_value("csl_status", "CSL Confirmed");
+  refresh: function(frm) {
+    if (frm.perm[0].write) {
+      if (frm.doc.status == "Pending for CSL") {
+        frm.add_custom_button(__("Confirm CSL"), function() {
+          frm.set_value("csl_status", "CSL Confirmed");
+          frm.save();
+        });
+      } else {
+        if (frm.doc.status != "Pending for PSL") {
+          frm.add_custom_button(__("revert to PSL"), function() {
+            frm.set_value("csl_status", "Pending for CSL");
             frm.save();
           });
         }
-        else {
-          if (frm.doc.status != "Pending for PSL") {
-            frm.add_custom_button(__("revert to PSL"), function () {
-              frm.set_value("csl_status", "Pending for CSL");
-              frm.save();
-            });
-          }
-        }
       }
-
-      client_pending = 0;
-      client_pending = frm.doc.client_sc - frm.doc.client_advance;
-      frm.set_value("client_pending", client_pending);
-
-      candidate_pending = 0;
-      candidate_pending = frm.doc.candidate_sc - frm.doc.candidate_advance;
-      frm.set_value("candidate_pending", candidate_pending);
-    },
-    onload: function (frm) {
-      frm.set_query("project", function () {
-        return {
-          query: "recruitment.recruitment.doctype.candidate.candidate.get_projects",
-          filters: {
-            customer: frm.doc.customer
-          }
-        };
-      });
-
-      frm.set_query("task", function () {
-        return {
-          query: "recruitment.recruitment.doctype.candidate.candidate.get_tasks",
-          filters: {
-            project: frm.doc.project
-          }
-        };
-      });
-
-      frm.set_query("candidate", function () {
-        return {
-          query: "recruitment.recruitment.doctype.candidate.candidate.get_candidates",
-          filters: {
-            task: frm.doc.task
-          }
-        };
-      });
-      /*
-       frm.set_query("dle", function() {
-       return {
-         query: "recruitment.recruitment.doctype.closure.closure.get_dle",
-         filters:{
-           candidate:frm.doc.candidate
-         }
-         }
-      });
-      
-        frm.set_query("tl", function() {
-      return {
-       query: "recruitment.recruitment.doctype.closure.closure.get_tl",
-       filters:{
-         dle:frm.doc.dle
-       }
-       }
-      });*/
     }
 
-  });
+    client_pending = 0;
+    client_pending = frm.doc.client_sc - frm.doc.client_advance;
+    frm.set_value("client_pending", client_pending);
+
+    candidate_pending = 0;
+    candidate_pending = frm.doc.candidate_sc - frm.doc.candidate_advance;
+    frm.set_value("candidate_pending", candidate_pending);
+  },
+  onload: function(frm) {
+    frm.set_query("project", function() {
+      return {
+        query: "recruitment.recruitment.doctype.candidate.candidate.get_projects",
+        filters: {
+          customer: frm.doc.customer
+        }
+      };
+    });
+
+    frm.set_query("task", function() {
+      return {
+        query: "recruitment.recruitment.doctype.candidate.candidate.get_tasks",
+        filters: {
+          project: frm.doc.project
+        }
+      };
+    });
+
+    frm.set_query("candidate", function() {
+      return {
+        query: "recruitment.recruitment.doctype.candidate.candidate.get_candidates",
+        filters: {
+          task: frm.doc.task
+        }
+      };
+    });
+    /*
+     frm.set_query("dle", function() {
+     return {
+       query: "recruitment.recruitment.doctype.closure.closure.get_dle",
+       filters:{
+         candidate:frm.doc.candidate
+       }
+       }
+    });
+
+      frm.set_query("tl", function() {
+    return {
+     query: "recruitment.recruitment.doctype.closure.closure.get_tl",
+     filters:{
+       dle:frm.doc.dle
+     }
+     }
+    });*/
+  }
+
+});
--- recruitment/recruitment/doctype/closure/closure.py
+++ recruitment/recruitment/doctype/closure/closure.py
@@ -32,7 +32,7 @@ class Closure(Document):
                     if self.csl_status == 'CSL Confirmed':
                         if self.premedical and self.offer_letter and self.mol:
                             if self.visa:
-                                if self.pcc and self.final_medical:
+                                if self.final_medical:
                                     if self.stamped_visa or self.poe or self.vhrs_affidavit:
                                         if self.payment_reciept:
                                             if self.ticket:
@@ -64,12 +64,12 @@ class Closure(Document):
                     self.csl_status = 'Pending for CSL'
                     self.status_updated_on = today()
 
-            elif self.territory == 'Dammam' or self.territory == 'Jeddah' or self.territory == 'Riyadh'or self.territory == 'Kuwait':
+            elif self.territory == 'Dammam' or self.territory == 'Jeddah' or self.territory == 'Riyadh':
                 if self.irf and self.passport and self.photo:
                     if self.csl_status == 'CSL Confirmed':
                         if self.offer_letter:
                             if self.visa:
-                                if self.pcc and self.final_medical:
+                                if self.final_medical:
                                     if self.stamped_visa or self.poe or self.vhrs_affidavit:
                                         if self.payment_reciept:
                                             if self.ticket:
@@ -101,13 +101,13 @@ class Closure(Document):
                     self.csl_status = 'Pending for CSL'
                     self.status_updated_on = today()
 
-            elif self.territory == 'Qatar' or self.territory == 'Oman':
+            elif self.territory == 'Qatar':
                 if self.irf and self.passport and self.photo:
                     if self.csl_status == 'CSL Confirmed':
                         if self.premedical and self.offer_letter:
                             if self.visa:
                                 if self.pcc:
-                                    if self.stamped_visa or self.poe or self.vhrs_affidavit:
+                                    if self.poe or self.vhrs_affidavit:
                                         if self.payment_reciept:
                                             if self.ticket:
                                                 self.status = 'Pending for Candidate Onboarding'
@@ -123,6 +123,39 @@ class Closure(Document):
                                         self.status_updated_on = today()
                                 else:
                                     self.status = 'Pending for PCC'
+                                    self.status_updated_on = today()
+                            else:
+                                self.status = 'Pending for Visa'
+                                self.status_updated_on = today()
+                        else:
+                            self.status = 'Pending for Premedical and offer Letter'
+                            self.status_updated_on = today()
+                    else:
+                        self.status = 'Pending for CSL'
+                        self.status_updated_on = today()
+                else:
+                    self.status = 'Pending for PSL'
+                    self.csl_status = 'Pending for CSL'
+                    self.status_updated_on = today()
+
+            elif self.territory == 'Oman':
+                if self.irf and self.passport and self.photo:
+                    if self.csl_status == 'CSL Confirmed':
+                        if self.premedical and self.offer_letter:
+                            if self.visa:
+                                if self.poe or self.vhrs_affidavit:
+                                    if self.payment_reciept:
+                                        if self.ticket:
+                                            self.status = 'Pending for Candidate Onboarding'
+                                            self.status_updated_on = today()
+                                        else:
+                                            self.status = 'Pending for Ticket Details'
+                                            self.status_updated_on = today()
+                                    else:
+                                        self.status = 'Pending for Payment Receipt'
+                                        self.status_updated_on = today()
+                                else:
+                                    self.status = 'Pending for Visa Stamping'
                                     self.status_updated_on = today()
                             else:
                                 self.status = 'Pending for Visa'
