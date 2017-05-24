772bcc2b13f61fb63744ece66bca602e2d9389d1
Abdulla <hereabdulla@gmail.com>
2017-05-24 10:21:55 +0530

    VHRS Changes @ 24.05.2017

--- recruitment/recruitment/doctype/candidate/candidate.js
+++ recruitment/recruitment/doctype/candidate/candidate.js
@@ -1,22 +1,26 @@
 frappe.ui.form.on('Candidate', {
-  refresh: function (frm) {
+  refresh: function(frm) {
     if (!frm.doc.user) {
       frm.set_value('user', frappe.session.user);
     }
 
   },
-  validate: function (frm) {
-    // clear linked customer / supplier / sales partner on saving...
+  validate: function(frm) {
     frm.toggle_reqd(["passport_no", "issued_date", "expiry_date", "place_of_issue", "ecr_status"],
       frm.doc.pending_for == 'Proposed PSL');
   },
-  associate_name: function (frm) {
+  associate_name: function(frm) {
     if (!frm.doc.associate_name) {
       frm.set_value('contact_no', '');
     }
   },
-  onload: function (frm) {
-    frm.set_query("project", function () {
+
+  'onload_post_render': function(frm) {
+    me = $(cur_frm.fields_dict.passport_no.input);
+    me.attr("maxlength", "8");
+  },
+  onload: function(frm) {
+    frm.set_query("project", function() {
       return {
         query: "recruitment.recruitment.doctype.candidate.candidate.get_projects",
         filters: {
@@ -25,7 +29,7 @@ frappe.ui.form.on('Candidate', {
       };
     });
 
-    frm.set_query("task", function () {
+    frm.set_query("task", function() {
       return {
         query: "recruitment.recruitment.doctype.candidate.candidate.get_tasks",
         filters: {
@@ -34,13 +38,19 @@ frappe.ui.form.on('Candidate', {
       };
     });
 
+    frm.set_query("associate_name", function() {
+      return {
+        query: "recruitment.recruitment.doctype.candidate.candidate.get_associates",
+        filters: {}
+      };
+    });
 
   }
 
 });
 
 frappe.ui.form.on('Task Candidate', {
-  refresh: function (frm) {
+  refresh: function(frm) {
     if (!frm.doc.user) {
       frm.set_value('user', frappe.session.user);
     }
