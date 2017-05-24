caeb5c55ef567d1dd8adc41a229cb2808b7e3bff
VHRS <abdulla.pi@voltechgroup.com>
2017-05-24 20:14:21 +0530

    nothing changed

--- recruitment/recruitment/doctype/candidate/candidate.js
+++ recruitment/recruitment/doctype/candidate/candidate.js
@@ -1,43 +1,46 @@
 frappe.ui.form.on('Candidate', {
-refresh: function(frm) {
-  if(!frm.doc.user) {
-    frm.set_value('user', frappe.session.user);
-  }
-  
-},
-associate_name:function(frm){
-    if(!frm.doc.associate_name) {
+  refresh: function(frm) {
+    if (!frm.doc.user) {
+      frm.set_value('user', frappe.session.user);
+    }
+
+  },
+  /*pending_for: function(frm) {
+    frm.toggle_reqd("irf", frm.doc.pending_for === "Proposed PSL");
+  },*/
+  associate_name: function(frm) {
+    if (!frm.doc.associate_name) {
       frm.set_value('contact_no', '');
     }
-},
- onload:function(frm){
-   frm.set_query("project", function() {
-	 return {
-		 query: "recruitment.recruitment.doctype.candidate.candidate.get_projects",
-		 filters:{
-       customer:frm.doc.customer
-		 }
-		 };
- });
+  },
+  onload: function(frm) {
+    frm.set_query("project", function() {
+      return {
+        query: "recruitment.recruitment.doctype.candidate.candidate.get_projects",
+        filters: {
+          customer: frm.doc.customer
+        }
+      };
+    });
 
-	 frm.set_query("task", function() {
-	 return {
-		 query: "recruitment.recruitment.doctype.candidate.candidate.get_tasks",
-		 filters:{
-       project:frm.doc.project
-		 }
-		 };
- });
+    frm.set_query("task", function() {
+      return {
+        query: "recruitment.recruitment.doctype.candidate.candidate.get_tasks",
+        filters: {
+          project: frm.doc.project
+        }
+      };
+    });
 
 
-}
+  }
 
 });
 
 frappe.ui.form.on('Task Candidate', {
-refresh: function(frm) {
-  if(!frm.doc.user) {
-    frm.set_value('user', frappe.session.user);
-  }
-},
+  refresh: function(frm) {
+    if (!frm.doc.user) {
+      frm.set_value('user', frappe.session.user);
+    }
+  },
 });
