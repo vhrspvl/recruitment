92ae1495c6b0df76b97ddfcad0e644b47fa5bd24
Abdulla <hereabdulla@gmail.com>
2017-05-30 11:59:49 +0530

    added filter to test status filter

--- recruitment/recruitment/doctype/closure/closure_list.js
+++ recruitment/recruitment/doctype/closure/closure_list.js
@@ -1,3 +1,8 @@
 frappe.listview_settings['Closure'] = {
-  add_fields: ["photo"]
-};
+  add_fields: ["photo"],
+  onload: function(me) {
+    frappe.route_options = {
+      "status": ["Pending for PSL", "Pending for Candidate Onboarding"]
+    };
+  }
+}
