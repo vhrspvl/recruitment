a036c9c271edad4d247aac7dff69a585475ab7af
Abdulla <hereabdulla@gmail.com>
2017-06-06 19:33:51 +0530

    Passport applied status added

--- recruitment/recruitment/doctype/candidate/candidate.js
+++ recruitment/recruitment/doctype/candidate/candidate.js
@@ -5,20 +5,35 @@ frappe.ui.form.on('Candidate', {
     }
   },
   validate: function(frm) {
-    frm.toggle_reqd(["passport_no", "expiry_date", "place_of_issue", "ecr_status"],
-      frm.doc.pending_for == 'Proposed PSL');
+    if (!frm.doc.applied && !frm.doc.not_applicable) {
+      frm.toggle_reqd(["passport_no", "expiry_date", "place_of_issue"],
+        frm.doc.pending_for == 'Proposed PSL');
+    }
+
   },
   associate_name: function(frm) {
     if (!frm.doc.associate_name) {
       frm.set_value('contact_no', '');
     }
   },
-
   'onload_post_render': function(frm) {
+    $(cur_frm.fields_dict.not_applicable.input).addClass('chb');
+    $(cur_frm.fields_dict.applied.input).addClass('chb');
+    $(".chb").on('change', function() {
+      $('.chb').not(this).prop('checked', false)
+    });
     me = $(cur_frm.fields_dict.passport_no.input);
     me.attr("maxlength", "8");
+
   },
   onload: function(frm) {
+    if (frm.doc.ecr_status && frm.doc.ecr_status == 'ECR') {
+      frm.doc.ecr = 1;
+      frm.doc.ecr_status = 'ECR';
+    } else {
+      frm.doc.ecr = 0;
+      frm.doc.ecr_status = 'ECNR';
+    }
     frm.set_query("project", function() {
       return {
         query: "recruitment.recruitment.doctype.candidate.candidate.get_projects",
