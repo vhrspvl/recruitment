7075d0f00334985240b838f6c7131a996d4a1025
Abdulla P I <hereabdulla@gmail.com>
2017-09-13 20:19:13 +0530

    Added Button for confirm sales invoice

--- recruitment/recruitment/doctype/closure/closure.js
+++ recruitment/recruitment/doctype/closure/closure.js
@@ -1,17 +1,18 @@
+cur_frm.add_fetch("project", "cpc", "cpc");
 cur_frm.add_fetch("customer", "customer_owner", "bde");
 
 frappe.ui.form.on('Closure', {
-  'onload_post_render': function(frm) {
+  'onload_post_render': function (frm) {
 
   },
 
-  candidate_boarded: function(frm) {
+  candidate_boarded: function (frm) {
     frm.set_value("status", "Onboarded");
     frm.save();
 
   },
 
-  refresh: function(frm) {
+  refresh: function (frm) {
     frm.toggle_display("poe", frm.doc.ecr_status === 'ECR');
     /*  var wrapper = frm.get_field("offer_letter_html").$wrapper;
       var is_viewable = frappe.utils.is_image_file(frm.doc.offer_letter);
@@ -27,7 +28,7 @@ frappe.ui.form.on('Closure', {
       }*/
 
     if (frm.doc.status == 'Onboarded' && frm.doc.territory != 'India') {
-      frm.add_custom_button(__("Revert to Pending"), function() {
+      frm.add_custom_button(__("Revert to Pending"), function () {
         frm.set_value("status", "Onboarding");
         frm.save();
       });
@@ -40,7 +41,7 @@ frappe.ui.form.on('Closure', {
     if (frm.perm[0].write) {
       if (frm.doc.status == "Sales Order") {
         if (!frm.doc.sales_order_confirmed_date && frappe.user.has_role("Project Leader")) {
-          frm.add_custom_button(__("Confirm Sales Order"), function() {
+          frm.add_custom_button(__("Confirm Sales Order"), function () {
             if (frm.doc.client_payment_applicable || frm.doc.candidate_payment_applicable) {
               if (frm.doc.client_payment_applicable && frm.doc.client_sc <= 0) {
                 msgprint("Please Enter Client Service Charge Value")
@@ -49,9 +50,8 @@ frappe.ui.form.on('Closure', {
               } else {
                 frappe.confirm(
                   'Did you verified the payment terms?',
-                  function() {
+                  function () {
                     frm.set_value("csl_status", "Sales Order Confirmed");
-                    frm.set_value("status", "Onboarded")
                     frm.set_value("sales_order_confirmed_date", frappe.datetime.get_today())
                     frm.save();
                   })
@@ -61,6 +61,19 @@ frappe.ui.form.on('Closure', {
             }
           });
         }
+      }
+      if (frm.doc.csl_status == 'Sales Order Confirmed') {
+        me = frm.add_custom_button(__("Confirm Sales Invoice"), function () {
+          frappe.confirm(
+            'Confirm Sales Invoice?',
+            function () {
+              frm.set_value("csl_status", "Sales Invoice Confirmed");
+              frm.set_value("sales_invoice_confirmed_date", frappe.datetime.get_today())
+              frm.save();
+            })
+        });
+        me.addClass('btn btn-primary');
+
       }
     }
 
@@ -72,8 +85,8 @@ frappe.ui.form.on('Closure', {
     candidate_pending = frm.doc.candidate_sc - frm.doc.candidate_advance;
     frm.set_value("candidate_pending", candidate_pending);
   },
-  onload: function(frm) {
-    frm.set_query("project", function() {
+  onload: function (frm) {
+    frm.set_query("project", function () {
       return {
         query: "recruitment.recruitment.doctype.candidate.candidate.get_projects",
         filters: {
@@ -82,7 +95,7 @@ frappe.ui.form.on('Closure', {
       };
     });
 
-    frm.set_query("task", function() {
+    frm.set_query("task", function () {
       return {
         query: "recruitment.recruitment.doctype.candidate.candidate.get_tasks",
         filters: {
@@ -91,7 +104,7 @@ frappe.ui.form.on('Closure', {
       };
     });
 
-    frm.set_query("candidate", function() {
+    frm.set_query("candidate", function () {
       return {
         query: "recruitment.recruitment.doctype.candidate.candidate.get_candidates",
         filters: {
