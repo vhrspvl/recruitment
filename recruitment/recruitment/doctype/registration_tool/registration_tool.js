65d2f09fcf5077b952a8082ff71c48b436a4d471
Abdulla P I <hereabdulla@gmail.com>
2017-10-06 16:42:42 +0530

    No Changes

--- recruitment/recruitment/doctype/registration_tool/registration_tool.js
+++ recruitment/recruitment/doctype/registration_tool/registration_tool.js
@@ -2,32 +2,32 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Registration Tool', {
-  refresh: function(frm) {
+  refresh: function (frm) {
     frm.disable_save();
   },
-  'onload_post_render': function(frm) {
+  'onload_post_render': function (frm) {
     me = $(cur_frm.fields_dict.term_1.input);
     $(cur_frm.fields_dict.have_pp.input).addClass('chb');
     $(cur_frm.fields_dict.not_applied_yet.input).addClass('chb');
     $(cur_frm.fields_dict.applied.input).addClass('chb');
-    $(".chb").change(function() {
+    $(".chb").change(function () {
       $(".chb").prop('checked', false);
       $(this).prop('checked', true);
     });
 
     $(cur_frm.fields_dict.relative_yes.input).addClass('chb1');
     $(cur_frm.fields_dict.relative_no.input).addClass('chb1');
-    $(".chb1").change(function() {
+    $(".chb1").change(function () {
       $(".chb1").prop('checked', false);
       $(this).prop('checked', true);
     });
 
   },
-  submit: function(frm) {
+  submit: function (frm) {
     frappe.call({
       method: "register",
       doc: frm.doc,
-      callback: function(r) {
+      callback: function (r) {
         var candidate = r.message;
         frappe.confirm(
           '<div class="panel panel-default">\
@@ -44,20 +44,20 @@ frappe.ui.form.on('Registration Tool', {
         </div>\
         </div>\
         </div>',
-          function() {
+          function () {
             frappe.prompt({
                 fieldtype: "Data",
                 label: __("Registration"),
                 fieldname: "testid",
               },
-              function(data) {
+              function (data) {
                 frappe.call({
                   method: "recruitment.api.confirm_register",
                   args: {
                     "testid": data.testid,
                     "doc": frm.doc
                   },
-                  callback: function(r) {
+                  callback: function (r) {
                     if (r.message == 'invalid') {
                       frappe.msgprint({
                         message: __('Entered Token is Invalid'),
@@ -69,11 +69,10 @@ frappe.ui.form.on('Registration Tool', {
                        <div><ul><li><h3>{0}<h3></li></ul></div></div>', [r.message])
                       frappe.confirm(
                         message,
-                        function() {
+                        function () {
                           location.reload();
                         })
                     }
-
                   }
                 });
               }, __("Enter Registration ID"), __("Submit"));
