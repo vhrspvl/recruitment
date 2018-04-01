7883967581856ef9934933f1e501c2282152c294
Abdulla P I <hereabdulla@gmail.com>
2018-04-01 19:56:26 +0530

    deleted dashboard

--- recruitment/config/desktop.py
+++ recruitment/config/desktop.py
@@ -1,23 +1,15 @@
 from __future__ import unicode_literals
 from frappe import _
 
+
 def get_data():
-	return [
-		{
-			"module_name": "Recruitment",
-			"label": _("Recruitment"),
-			"color": "#FFF5A7",
-			"reverse": 1,
-			"icon": "octicon octicon-calendar",
-			"type": "module"
-		},
-		{
-			"module_name": "Recruitment",
-			"color": "#589494",
-			"icon": "fa fa-th",
-			"icon": "octicon octicon dashboard",
-			"type": "page",
-			"link": "dashboard",
-			"label": _("Dashboard")
-		}
-	]
+    return [
+        {
+            "module_name": "Recruitment",
+            "label": _("Recruitment"),
+            "color": "#FFF5A7",
+            "reverse": 1,
+            "icon": "octicon octicon-calendar",
+            "type": "module"
+        }
+    ]
